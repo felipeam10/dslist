6ff3187ecf6b314b70281b5191a05f8bc3343523
felipeam10 <felipeam10@hotmail.com>
2023-05-15 16:47:02 -0300

    Consultas SQL no Spring data JPA and Projections

--- src/main/java/com/gamesdevsup/dslist/services/GameService.java
+++ src/main/java/com/gamesdevsup/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.gamesdevsup.dslist.services;
 import com.gamesdevsup.dslist.dto.GameDTO;
 import com.gamesdevsup.dslist.dto.GameMinDTO;
 import com.gamesdevsup.dslist.entities.Game;
+import com.gamesdevsup.dslist.projections.GameMinProjection;
 import com.gamesdevsup.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+
+    }
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
 
     }
